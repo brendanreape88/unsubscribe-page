27ab1a286283f809ff91beecfe31d8c884fb0c4a
Brendan Reape <brendanreape88@gmail.com>
2021-11-22 21:16:19 +0000

    added dynamic banner and adjusted sizing

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 // Selectors
 const unsubscribeButton = document.querySelector(".unsubscribe_button");
+const logoURL = getUrlVars()["inf_custom_LogoURL"];
+const decodelogoURL = decodeURIComponent(logoURL);
 
 // Event Listeners
 unsubscribeButton.addEventListener("click", unsubscribe);
 
 // Functions
+window.onload = function () {
+  getUrlVars();
+};
+
 function unsubscribe() {
   document.getElementById("pre_unsubscribe_container").style.display = "none";
   document.getElementById("post_unsubscribe_container").style.display = "flex";
   document.getElementById("unsubscribe_button").style.visibility = "hidden";
 }
+
+function getUrlVars() {
+  var vars = {};
+  var parts = window.location.href.replace(
+    /[?&]+([^=&]+)=([^&]*)/gi,
+    function (m, key, value) {
+      vars[key] = value;
+    }
+  );
+  return vars;
+}
+
+document.getElementById("banner").src = decodelogoURL;
